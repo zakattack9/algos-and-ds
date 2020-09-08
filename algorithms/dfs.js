fc7fa928bd899969093a2abcd1e69fa962326aea
zakattack9 <zakattack26111@gmail.com>
2020-09-07 20:35:51 -1000

    updated dfs

--- algorithms/dfs.js
+++ algorithms/dfs.js
@@ -32,6 +32,71 @@ export const DFSPostOrder = (root, traversal = []) => {
   return traversal;
 }
 
+// in-order iterative depth-first search solution
+export const DFSInOrderIterative = (root) => {
+  let traversal = [];
+  if (!root) return traversal;
+
+  let stack = [];
+  let node = root;
+
+  while (node || stack.length) {
+    if (node) {
+      stack.push(node);
+      node = node.left;
+    } else {
+      node = stack.pop();
+      traversal.push(node.value);
+      node = node.right;
+    }
+  }
+
+  return traversal;
+}
+
+//  pre-order iterative depth-first search solution
+export const DFSPreOrderIterative = (root) => {
+  let traversal = [];
+  if (!root) return traversal;
+
+  let stack = [root];
+  let node;
+
+  while(stack.length) {
+    node = stack.pop();
+    traversal.push(node.value);
+
+    if (node.right) stack.push(node.right);
+    if (node.left) stack.push(node.left);
+  }
+
+  return traversal;
+}
+
+// post-order iterative depth-first search solution
+export const DFSPostOrderIterative = (root) => {
+  let traversal = [];
+  if (!root) return traversal;
+
+  let stack = [root];
+  let node;
+
+  while(stack.length) {
+    node = stack.pop();
+    traversal.push(node.value);
+
+    if (node.left) stack.push(node.left);
+    if (node.right) stack.push(node.right);
+  }
+
+  return traversal.reverse();
+}
+
+// one liners
+export const DFS_In = root => root ? [...DFS_In(root.left), root.value, ...DFS_In(root.right)] : [];
+export const DFS_Pre = root => root ? [root.value, ...DFS_Pre(root.left), ...DFS_Pre(root.right)] : [];
+export const DFS_Post = root => root ? [...DFS_Post(root.left), ...DFS_Post(root.right), root.value] : [];
+
 // export const DFSRightSide = (root, traversal = []) => {
 
 // }
--- algorithms/test-algos.js
+++ algorithms/test-algos.js
@@ -2,7 +2,7 @@ import Quicksort from './quicksort.js';
 
 import { buildBinaryTree } from '../data-structures/binarytreenode.js';
 import BFS, { BFSRightSide, BFSLeftSide } from './bfs.js';
-import DFS, { DFSPreOrder, DFSPostOrder } from './dfs.js';
+import DFS, { DFSPreOrder, DFSPostOrder, DFSInOrderIterative, DFSPreOrderIterative, DFSPostOrderIterative } from './dfs.js';
 import BinarySearch from './binarysearch.js';
 
 const UNSORTED_ARR = [1, 5, 2, 9, 4, 3, 7, 0, 6, 8];
@@ -33,6 +33,15 @@ console.log('DFS PREORDER TRAVERSAL', dfsPreOrderTraversal);
 
 const dfsPostOrderTraversal = DFSPostOrder(root);
 console.log('DFS POSTORDER TRAVERSAL', dfsPostOrderTraversal);
+
+const dfsInOrderIterativeTraversal = DFSInOrderIterative(root);
+console.log('DFS INORDER ITERATIVE TRAVERSAL', dfsInOrderIterativeTraversal);
+
+const dfsPreOrderIterativeTraversal = DFSPreOrderIterative(root);
+console.log('DFS PREORDER ITERATIVE TRAVERSAL', dfsPreOrderIterativeTraversal);
+
+const dfsPostOrderIterativeTraversal = DFSPostOrderIterative(root);
+console.log('DFS POSTORDER ITERATIVE TRAVERSAL', dfsPostOrderIterativeTraversal);
 
 // binary search
 const bsIndex = BinarySearch(quicksortArr, 2);
